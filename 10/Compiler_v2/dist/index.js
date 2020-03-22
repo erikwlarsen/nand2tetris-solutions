fdb3f1e583b109514ae4fdc0d9fc50373ce96784
Erik Larsen <erik.w.larsen@gmail.com>
2020-03-22 14:08:10 -0400

    add working index file to compiler v2

--- 10/Compiler_v2/dist/index.js
+++ 10/Compiler_v2/dist/index.js
@@ -2,12 +2,46 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const tokenize_1 = __importDefault(require("./tokenize"));
-const fs_1 = require("fs");
-const path_1 = require("path");
-console.log('bloop');
-const text = fs_1.readFileSync(path_1.resolve(__dirname, '../../ArrayTest/Main.jack')).toString();
-console.log(text);
-console.log(tokenize_1.default(text));
+const fs = __importStar(require("fs"));
+const path = __importStar(require("path"));
+try {
+    const [, , filePath] = process.argv;
+    const fullPath = path.resolve(process.cwd(), filePath);
+    if (!fs.existsSync(fullPath)) {
+        throw new Error(`could not find file or directory at path ${fullPath}`);
+    }
+    const filesToProcess = [];
+    const stats = fs.lstatSync(fullPath);
+    if (stats.isDirectory()) {
+        const files = fs.readdirSync(fullPath);
+        files.forEach(file => filesToProcess.push(`${fullPath}/${file}`));
+    }
+    else {
+        filesToProcess.push(fullPath);
+    }
+    filesToProcess.forEach((fPath) => {
+        const pathParts = fPath.split('.');
+        const extension = pathParts.pop();
+        if (extension !== 'jack') {
+            console.log(`Input file ${filePath}.${extension} does not have jack extension, skipping`);
+            return;
+        }
+        const text = fs.readFileSync(fPath, 'utf8');
+        const output = tokenize_1.default(text);
+        fs.writeFileSync(pathParts.join('.').concat('.json'), JSON.stringify(output, null, 2));
+    });
+}
+catch (e) {
+    console.error(e);
+    process.exit(1);
+}
 //# sourceMappingURL=index.js.map
--- 10/Compiler_v2/src/index.ts
+++ 10/Compiler_v2/src/index.ts
@@ -1,7 +1,33 @@
 import tokenize from './tokenize';
-import { readFileSync } from 'fs';
-import { resolve } from 'path';
-console.log('bloop'); 
-const text = readFileSync(resolve(__dirname, '../../ArrayTest/Main.jack')).toString();
-console.log(text);
-console.log(tokenize(text));
+import * as fs from 'fs';
+import * as path from 'path';
+
+try {
+  const [,, filePath] = process.argv;
+  const fullPath = path.resolve(process.cwd(), filePath);
+  if (!fs.existsSync(fullPath)) {
+    throw new Error(`could not find file or directory at path ${fullPath}`);
+  }
+  const filesToProcess: string[] = [];
+  const stats = fs.lstatSync(fullPath);
+  if (stats.isDirectory()) {
+    const files = fs.readdirSync(fullPath);
+    files.forEach(file => filesToProcess.push(`${fullPath}/${file}`));
+  } else {
+    filesToProcess.push(fullPath);
+  }
+  filesToProcess.forEach((fPath) =>{
+    const pathParts = fPath.split('.');
+      const extension = pathParts.pop();
+      if (extension !== 'jack') {
+        console.log(`Input file ${filePath}.${extension} does not have jack extension, skipping`);
+        return;
+      }
+      const text = fs.readFileSync(fPath, 'utf8');
+      const output = tokenize(text);
+      fs.writeFileSync(pathParts.join('.').concat('.json'), JSON.stringify(output, null, 2));
+  });
+} catch (e) {
+  console.error(e);
+  process.exit(1);
+}
